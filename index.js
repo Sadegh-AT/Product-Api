5954d73b89bad4fa5f61fb5c96eceb50983d3dcb
SadeghAT <sadegh.artivox@gmail.com>
2023-07-24 12:30:22 +0330

    getter setter class

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 class User {
   #name;
   constructor(name) {
-    this.#setName(name);
-  }
-
-  #setName(name) {
     this.#name = name;
   }
-  getName() {
+
+  get Name() {
     return this.#name;
   }
-  static printInfo() {
-    console.log(this.name);
+  set Name(name) {
+    if (!name) {
+      console.log("Error");
+    } else {
+      this.#name = name;
+    }
   }
 }
 
 const user1 = new User("Sadegh");
 
-User.printInfo();
-console.log(user1.getName());
+console.log(user1.getName);
