01ef543d7cabb3b9749ad22707d17a16942a7d9a
SadeghAT <sadegh.artivox@gmail.com>
2023-07-24 12:24:55 +0330

    class access modify

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import { getUserBasket } from "./user-basket.js";
+class User {
+  #name;
+  constructor(name) {
+    this.#setName(name);
+  }
 
-getUserBasket();
+  #setName(name) {
+    this.#name = name;
+  }
+  getName() {
+    return this.#name;
+  }
+  static printInfo() {
+    console.log(this.name);
+  }
+}
+
+const user1 = new User("Sadegh");
+
+User.printInfo();
+console.log(user1.getName());
