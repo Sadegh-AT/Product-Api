0ad31660eeb697420d6a60a62d407b10b8d4310f
SadeghAT <sadegh.artivox@gmail.com>
2023-07-27 15:58:30 +0330

    Content-Type

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const http = require("http");
-
+let users = [
+  { id: 1, name: "sadegh" },
+  { id: 2, name: "wd" },
+  { id: 3, name: "s" },
+];
 http
   .createServer((req, res) => {
-    res.write(req.url);
-    res.end();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(users));
   })
   .listen(3000, () => {
+    console.clear();
     console.log("Port: " + 3000);
   });
