f91a07cd0603512652a39c37dcc3850dac5ae83e
SadeghAT <sadegh.artivox@gmail.com>
2023-07-27 16:27:53 +0330

    product mini project

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const http = require("http");
-let users = [
-  { id: 1, name: "sadegh" },
-  { id: 2, name: "wd" },
-  { id: 3, name: "s" },
-];
+const product = require("./data/product.json");
+
+const PORT = 3000;
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    if (req.url == "/api/product/name") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      let productName = product.map((item) => item.name);
+      res.end(JSON.stringify(productName, null, 4));
+    } else if (req.url == "/api/product/info") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      let productInfo = product.map((item) => item.info);
+      res.end(JSON.stringify(productInfo, null, 4));
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found");
+    }
   })
-  .listen(3000, () => {
-    console.clear();
-    console.log("Port: " + 3000);
+  .listen(PORT, () => {
+    console.log(`run server on port ${PORT} http://localhost:${PORT}`);
   });
