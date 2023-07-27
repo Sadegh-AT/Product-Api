dfb7e9087605eca0ac8fe2553495d317972b8182
SadeghAT <sadegh.artivox@gmail.com>
2023-07-27 16:40:09 +0330

    controllers

--- controllers/product.controllers.js
+++ controllers/product.controllers.js
@@ -1,9 +1,15 @@
 const ProductModel = require("../model/product.model");
 
-async function getProductControllers(req, res) {
+async function get(req, res) {
   try {
     const products = await ProductModel.find();
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(products));
+    res.end(JSON.stringify(products, null, 4));
   } catch (error) {}
 }
+
+const ProductController = {
+  get,
+};
+
+module.exports = ProductController;
--- index.js
+++ index.js
@@ -1,21 +1,13 @@
 const http = require("http");
 const product = require("./data/product.json");
+const ProductController = require("./controllers/product.controllers");
 
 const PORT = 3000;
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    if (req.url == "/api/product/name") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      let productName = product.map((item) => item.name);
-      res.end(JSON.stringify(productName, null, 4));
-    } else if (req.url == "/api/product/info") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      let productInfo = product.map((item) => item.info);
-      res.end(JSON.stringify(productInfo, null, 4));
+    if (req.url == "/api/product") {
+      ProductController.get(req, res);
     } else {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      res.end("Not Found");
     }
   })
   .listen(PORT, () => {
